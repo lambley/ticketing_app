5d97e8a4b7f3670d437dee3b6315f136e14a33e4
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-16 16:06:33 +0100

    refactor custom error object to return the same structure for all error types

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,16 +1,44 @@
 // see "Writing error handlers" https://expressjs.com/en/guide/error-handling.html
-
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
+// custom Error object (array of objects)
+// errors: {
+//   message: string,
+//   field?: string
+// } []
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err);
+  // request validation errors
+  if (err instanceof RequestValidationError) {
+    // for each error, change error custom Error
+    const formattedErrors = err.errors.map((error) => {
+      return {
+        message: error.msg,
+        field: error.param,
+      };
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
 
+  // database connection errors
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({
+      errors: [
+        {
+          message: err.reason,
+        },
+      ],
+    });
+  }
+
+  // unexpected errors
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: 'Unexpected error occurred' }],
   });
 };
