027fafd98083a49b881a672f7b33469a4e3def4d
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-16 12:36:22 +0100

    create basic error handling middleware

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,16 @@
+// see "Writing error handlers" https://expressjs.com/en/guide/error-handling.html
+
+import { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log('Something went wrong', err);
+
+  res.status(400).send({
+    message: 'Something went wrong',
+  });
+};
