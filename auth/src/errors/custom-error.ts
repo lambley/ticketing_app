265c7d49eb79cc6ee3582fc12768d61f22c80a12
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-16 16:34:25 +0100

    create abstract class for error subclass validation

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,14 @@
+// abstract class for our custom error subclasses
+// implenting this to minimise duplicated code in error-handler middleware
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+
+  constructor() {
+    super();
+
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  // define method signature for returned error message
+  abstract serializeErrors(): { message: string; field?: string }[];
+}
