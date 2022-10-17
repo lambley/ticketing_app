5cd5641265a4ecb2eb1d0614387dc18fefa558f6
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-17 11:38:34 +0100

    add interfaces to safely add typing to mongo User model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,7 +1,22 @@
 import mongoose from 'mongoose';
 
 // User model interface - for User attributes
+// interface for user model - all instances must have these fields
 interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// User model interface - for User properties
+// extending mongoose.Model and including build method - for TS interface
+// interface describing functions for User Collection
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// User Document interface
+// interface for describing a single User's properties
+interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
@@ -17,14 +32,19 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
-
-// compile User model and export
-const User = mongoose.model('User', userSchema);
-
-// function involve TypeScript in User creation - TS will check attrs
-// use this function to create new User instances, instead of creating new instances the mongoose way
-const buildUser = (attrs: UserAttrs) => {
+// refactor buildUser as statics
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
+
+// compile User model and export
+// add interfaces for UserDoc and UserModel
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+// build a new User like this
+// const user = User.build({
+//   email: 'test@test.com',
+//   password: 'pass',
+// });
 
 export { User };
