3e177e091b3be0a697ac181c7ae6c8879bc2043d
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-17 11:25:10 +0100

    add interface to involve TS in creating new instances

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,11 @@
 import mongoose from 'mongoose';
 
+// User model interface - for User attributes
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
 // mongoDB User schema
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,5 +20,11 @@ const userSchema = new mongoose.Schema({
 
 // compile User model and export
 const User = mongoose.model('User', userSchema);
+
+// function involve TypeScript in User creation - TS will check attrs
+// use this function to create new User instances, instead of creating new instances the mongoose way
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
 
 export { User };
