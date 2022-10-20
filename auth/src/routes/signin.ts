3e24576f7e2c7c1cde1fd6d972c117e293f066e4
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-20 17:33:12 +0100

    method to validate login details

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from 'express';
-
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('signin route');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password must be present'),
+  ],
+  (req: Request, res: Response) => {
+    // check if user exists, if not throw error
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+      throw new RequestValidationError(errors.array());
+    }
+    // compare password hashes
+    // create JWT
+  }
+);
 
 export { router as signinRouter };
