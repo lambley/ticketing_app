32e603edcfceb91f78d57e8ae293093370b64f93
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-21 10:21:04 +0100

    add signout - destroy session

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('signout route');
+  // destroy current session
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
