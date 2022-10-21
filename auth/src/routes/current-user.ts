5f2e7cc965f338ef6ef839f040b4349909937e85
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-21 11:00:59 +0100

    implement currentUser middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,24 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  // if no session or no JWT set
-  // .session? checks if session exists as well - equivalent to an || check
-  if (!req.session?.jwt) {
-    // send response: no current user
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    // decode JWT data - pass JWT and secret key
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    // send user email and id
-    res.send({ currentUser: payload });
-  } catch (error) {
-    res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
