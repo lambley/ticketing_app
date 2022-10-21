fbb94bfce4329221a7769318ad2234f7443fc1e8
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-21 10:14:34 +0100

    add current user logic - checks if a user is logged in

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('current user route');
+  // if no session or no JWT set
+  // .session? checks if session exists as well - equivalent to an || check
+  if (!req.session?.jwt) {
+    // send response: no current user
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    // decode JWT data - pass JWT and secret key
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    // send user email and id
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
