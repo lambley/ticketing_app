22e619836e3d65dc38b34d4bfc026252fb7097de
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-21 13:50:00 +0100

    add email and login tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,45 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 for invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'asdasdas',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 for invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'one',
+    })
+    .expect(400);
+});
+
+it('returns a 400 if no email or password supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400);
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: 'password' })
+    .expect(400);
+  await request(app).post('/api/users/signup').send({}).expect(400);
+});
+
+// it('returns a 400 if user already exists in database', async () => {
+//   return request(app)
+//     .post('/api/users/signup')
+//     .send({
+//       email: 'test@test.com',
+//       password: 'password',
+//     })
+//     .expect(400);
+// });
