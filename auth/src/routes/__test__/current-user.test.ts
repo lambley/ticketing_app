a7265fc61b96d8641e3111ae6a61e8ff41b2a86d
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-21 15:42:32 +0100

    add current-user test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,21 +2,13 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details on current signed in user', async () => {
-  const userEmail = 'test@test.com';
-  const authRes = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: userEmail,
-      password: 'password',
-    })
-    .expect(201);
-  const cookie = authRes.get('Set-Cookie');
+  const cookie = await global.signup();
 
   const res = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
     .expect(200);
-    
-  expect(res.body.currentUser.email).toEqual(userEmail);
+
+  expect(res.body.currentUser.email).toEqual(testEmail);
 });
