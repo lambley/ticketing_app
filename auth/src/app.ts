ab5b2171dd2e3af56663b4dfc89d6036bd1dba37
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-21 13:09:54 +0100

    refactor: create express app in app.ts and listen in index.ts

---
+++ auth/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+// allow ingress-nginx proxy for https connection
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    // disable encryption
+    signed: false,
+    // require https connections
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// Status 404: throw error for Not Found Routes
+// Note: would normally need async... next syntax for Express, but doesn't need it here due to express-async-errors module
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,43 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 import { JwtNotDefinedError } from './errors/jwt-not-defined';
 
-const app = express();
-// allow ingress-nginx proxy for https connection
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    // disable encryption
-    signed: false,
-    // require https connections
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// Status 404: throw error for Not Found Routes
-// Note: would normally need async... next syntax for Express, but doesn't need it here due to express-async-errors module
-app.all('*', async (req, res, next) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 // Mongo instance startup function
 // Connect to auth-mongo-srv ClusterIP
