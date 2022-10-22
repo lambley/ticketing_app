95b43a2f86ced969f61e16cdda22bd918e6b75f9
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-22 10:59:55 +0100

    add submit handler

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,19 +1,41 @@
 // route: /auth/signup
+import { useState } from 'react';
+
 const Signup = () => {
-  return(
-    <form action="">
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email);
+    console.log(password);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label htmlFor="">Email Address</label>
-        <input type="text" className="form-control" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="form-group">
         <label htmlFor="">Password</label>
-        <input type="password" className="form-control" />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
