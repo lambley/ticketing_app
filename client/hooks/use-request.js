5f151a20ffa0729816a1ab20221ca3b0d3b29d33
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-24 10:52:56 +0100

    change to throw custom error

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -19,7 +19,7 @@ const useRequest = ({ url, method, body, onSuccess }) => {
       return response.data;
     } catch (error) {
       setErrors(error.response.data.errors);
-      throw error;
+      throw error.response.data.errors;
     }
   };
 
