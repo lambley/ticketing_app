924b4b0e52e6a23482c2a8c24e2dc39f33a150ac
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-25 11:13:19 +0100

    refactor to make request more readable

--- client/pages/index.js
+++ client/pages/index.js
@@ -11,7 +11,8 @@ const Index = ({ currentUser }) => {
 };
 
 Index.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get('/api/users/currentuser');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
   return data;
 };
