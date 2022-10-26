d9fd322a73490d746c7046af32d5965d10d34bfa
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-26 14:56:10 +0100

    add Next.js AppComponent level getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,24 @@
 // custom app wrapper to include boostrap globally
 // see here for more details: https://nextjs.org/docs/basic-features/built-in-css-support
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+import buildClient from '../api/build-client';
 
 const _App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <h1>Header</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default _App
+_App.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default _App;
