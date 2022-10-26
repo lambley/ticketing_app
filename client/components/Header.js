b705d84f24727dbc64136c271d877988afd3c21f
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-26 15:24:24 +0100

    create a links array for showing signin/out links

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,6 +1,21 @@
 import Link from 'next/link';
 
 const Header = ({ currentUser }) => {
+  // create links array with 2 possible states:
+  // user signed in =
+  // [false, false, { label: 'Sign Out', href: '/auth/signout' }];
+  // user not signed in =
+  // [{ label: 'Sign Up', href: '/auth/signup' },{ label: 'Sign In', href: '/auth/signin' },false]
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return <li key={href}>{label}</li>;
+    });
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
@@ -10,9 +25,7 @@ const Header = ({ currentUser }) => {
       </Link>
 
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? 'Sign out' : 'Sign in/Sign up'}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
