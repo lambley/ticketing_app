cbdd2308ba411e403083e7859da96675987b9299
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-26 15:32:22 +0100

    create signout component to control signout logic

---
+++ client/pages/auth/signout.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
+
+const Signout = () => {
+  // navigate to home page on signout
+  const { doRequest } = useRequest({
+    url: '/api/users/signout',
+    method: 'post',
+    body: {},
+    onSuccess: () => Router.push('/'),
+  });
+
+  useEffect(() => {
+    doRequest();
+  }, []);
+
+  return <div>Signing out...</div>;
+};
+
+export default Signout;
