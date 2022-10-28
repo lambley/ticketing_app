cd43567e44fe8cb46c612098213a4634f21a09cb
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-28 10:52:16 +0100

    add mongodb uri to env and update database reference on start method

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -11,8 +11,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new JwtNotDefinedError();
   }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('connected to mongodb/auth');
   } catch (error) {
     console.error(error);
