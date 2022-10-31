89f0af82f49f719ec7b2121e919614910740abfb
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-31 09:42:18 +0000

    add price validation

--- ticket/src/routes/new.ts
+++ ticket/src/routes/new.ts
@@ -10,7 +10,12 @@ const router = express.Router();
 router.post(
   '/api/ticket',
   requireAuth,
-  [body('title').not().isEmpty().withMessage('Title is required')],
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
   validateRequest,
   (req: Request, res: Response) => {
     res.sendStatus(200);
