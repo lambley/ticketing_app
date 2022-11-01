25b513a465eee604444d867272208f58f5ba5a6e
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-01 09:56:28 +0000

    add show logic

--- ticket/src/routes/show.ts
+++ ticket/src/routes/show.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
+import { NotFoundError } from '@lambley-ticketing/ticketing-common/build';
 
 const router = express.Router();
 
-router.get('/api/tickets/:id', async (req: Request, res: Response) => {});
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  res.send(ticket);
+});
 
 export { router as showTicketRouter };
