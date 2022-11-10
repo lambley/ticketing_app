6c0c4bbfd6794630d756f7716df160cb1aa40f00
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-10 14:38:33 +0000

    add method to connect to nats client and return promise

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,7 +1,23 @@
 import nats, { Stan } from 'node-nats-streaming';
 
 class NatsWrapper {
-  
+  // _client is not initialised here, but in the start() function in index.ts
+  private _client?: Stan;
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      // on connect, log connection
+      this._client!.on('connect', () => {
+        console.log('Connected to NATS');
+        resolve();
+      });
+      this._client!.on('error', (err) => {
+        reject(err);
+      });
+    });
+  }
 }
 
 export const natsWrapper = new NatsWrapper();
