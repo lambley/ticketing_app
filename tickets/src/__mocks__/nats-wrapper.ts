a8ef75e285e1890cdf629a7810b244ebcbb5dc7c
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-23 15:21:04 +0000

    create nats-wrapper mock for tests

---
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,9 @@
+// mocking an instance of Nats Client, with Nats client property
+export const natsWrapper = {
+  // create a mock client with publish method
+  client: {
+    publish: (subject: string, data: string, callback: () => void) => {
+      callback();
+    },
+  },
+};
